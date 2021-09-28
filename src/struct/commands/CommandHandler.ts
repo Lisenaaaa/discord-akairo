969efbb942fed7555f9506c1367c5add9ea33e85
Lisena <lisenawastaken@gmail.com>
2021-09-27 19:23:28 -0700

    i hate subcommands

--- src/struct/commands/CommandHandler.ts
+++ src/struct/commands/CommandHandler.ts
@@ -768,44 +768,23 @@ export default class CommandHandler extends AkairoHandler {
 									convertedOptions[subOption.name] = subOption.role;
 									break;
 								case "MENTIONABLE":
-									convertedOptions[subOption.name] = subOption.role ? subOption.role : { user: subOption.user, member: subOption.member };
+									convertedOptions[subOption.name] = subOption.role
+										? subOption.role
+										: { user: subOption.user, member: subOption.member };
 									break;
 							}
-						})
+						});
 						break;
-					case "SUB_COMMAND_GROUP":{
-						const options = (option.options as CommandInteractionOption[])
-						convertedOptions["subcommand"] = (option.options as { name: string; type: string }[])[0].name;
+					case "SUB_COMMAND_GROUP": {
+						const options = option.options;
+						convertedOptions["subcommand"] = (
+							option.options as { name: string; type: string; options: CommandInteractionOption[] }[]
+						)[0].name;
 
-						options.forEach(subOption => {
-							switch (subOption.type) {
-								case "STRING":
-									convertedOptions[subOption.name] = subOption.value;
-									break;
-								case "INTEGER":
-									convertedOptions[subOption.name] = subOption.value;
-									break;
-								case "BOOLEAN":
-									convertedOptions[subOption.name] = subOption.value;
-									break;
-								case "NUMBER":
-									convertedOptions[subOption.name] = subOption.value;
-									break;
-								case "USER":
-									convertedOptions[subOption.name] = { user: subOption.user, member: subOption.member };
-									break;
-								case "CHANNEL":
-									convertedOptions[subOption.name] = subOption.channel;
-									break;
-								case "ROLE":
-									convertedOptions[subOption.name] = subOption.role;
-									break;
-								case "MENTIONABLE":
-									convertedOptions[subOption.name] = subOption.role ? subOption.role : { user: subOption.user, member: subOption.member };
-									break;
-							}
-						})
-						break}
+						convertedOptions['options'] = options
+
+						break;
+					}
 				}
 				// convertedOptions[option.name] = option
 			});
